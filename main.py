6c74d4058fa7fc60023ce5479f5bd86311e51669
BChain <browndj24@gmail.com>
2025-10-06 13:21:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 from pprint import pprint
-import streamlit as st
 import talib
-import yfinance as yf
-
 
 
 def get_sp500_tickers() -> tuple[list[str], dict[str, str]]:
@@ -13,20 +10,23 @@ def get_sp500_tickers() -> tuple[list[str], dict[str, str]]:
 
     # Add headers
     headers = {
-        'User-Agent': 
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
-    df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", 
-                      storage_options=headers)[0]
+    df = pd.read_html(
+        "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",
+        storage_options=headers,
+    )[0]
     tickers = df["Symbol"].tolist()
     tickers_dict = dict(zip(df["Symbol"], df["Security"]))
     return tickers, tickers_dict
+
 
 indicators = [
     "Exponential Moving Average",
     "Volume Weighted Average Price",
     "Relative Strength Index",
 ]
+
 
 def attach_indicator(indicator: str, data: pd.DataFrame) -> pd.DataFrame:
     """
